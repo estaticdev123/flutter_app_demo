c8710633ef3d8d4a04a4ca494ad51067fd8b4cc2
pm3@estatic-infotech.in <Estatic123$#>
2019-05-27 17:45:37 +0530

    commit
    initial commit

---
+++ flutter_app_demo/lib/pic_editor/select_pic.dart
@@ -0,0 +1,98 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+main() => runApp(SelectPic());
+
+class SelectPic extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      title: "Pic Edit",
+      debugShowCheckedModeBanner: false,
+      home: MainPage(),
+    );
+  }
+}
+
+class MainPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return Page();
+  }
+}
+
+class Page extends State<MainPage> {
+  Future<File> imageFile;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Text(
+          "EDIT YOUR PIC",
+          style:
+              TextStyle(fontSize: 16, fontFamily: "Acme", color: Colors.black),
+        ),
+      ),
+      body: new Center(
+        child: Container(
+          child: Column(
+            children: <Widget>[
+              showImage(),
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.white,
+        onPressed: () {
+          pickImageFromGallery(ImageSource.gallery);
+        },
+        child: Icon(
+          Icons.camera,
+          color: Color(0xFF13B0A5),
+          size: 50,
+        ),
+      ),
+    );
+  }
+
+  Widget showImage() {
+    return FutureBuilder<File>(
+      future: imageFile,
+      builder: (BuildContext context, AsyncSnapshot<File> snapshot) {
+        if (snapshot.connectionState == ConnectionState.done &&
+            snapshot.data != null) {
+          return Image.file(
+            snapshot.data,
+            width: 300,
+            height: 300,
+          );
+        } else if (snapshot.error != null) {
+          return const Text(
+            'Error Picking Image',
+            textAlign: TextAlign.center,
+          );
+        } else {
+          return const Text(
+            'No Image Selected',
+            textAlign: TextAlign.center,
+          );
+        }
+      },
+    );
+  }
+
+  pickImageFromGallery(ImageSource source) {
+    setState(() {
+      imageFile = ImagePicker.pickImage(source: source);
+    });
+  }
+}
+
+// Color(0xFFF1B136)
